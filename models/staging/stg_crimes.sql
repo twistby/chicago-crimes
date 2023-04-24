b151c6e24ad218c77ee079f1bdeaf471d2d524c1
pref@outlook.com <pref@outlook.com>
2023-04-24 18:04:12 +0000

    minor fix

--- models/staging/stg_crimes.sql
+++ models/staging/stg_crimes.sql
@@ -1,15 +1,15 @@
-{{config(materialized='view')}}
+{{ config(materialized="view") }}
 
-SELECT 
-    id, 
-    date, 
-    primary_type, 
-    description, 
-    CONCAT(primary_type, ' ', description) AS full_description, 
-    location_description, 
-    year, 
-    latitude, 
-    longitude 
-FROM {{ source('staging','crimes') }}
-WHERE latitude != 0 and longitude!= 0 and location_description = 'STREET' 
-LIMIT 1000
+select
+    id,
+    date,
+    primary_type,
+    description,
+    concat(primary_type, ' ', description) as full_description,
+    location_description,
+    year,
+    latitude,
+    longitude
+from {{ source("staging", "crimes") }}
+where latitude != 0 and longitude != 0 and location_description = 'STREET'
+limit 1000
