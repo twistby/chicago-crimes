25c7099e56319cf9758a0c1fc2a1cb199db6ff18
pref@outlook.com <pref@outlook.com>
2023-04-24 20:50:48 +0000

    minor fix

--- models/core/crimes_around_schools.sql
+++ models/core/crimes_around_schools.sql
@@ -2,8 +2,16 @@
 
 SELECT 
   c.id AS crime_id,
+  c.date,
+  c.primary_type,
+  c.description,
+  c.full_description,
+  c.year,
   s.school_id AS school_id,
-  ST_DISTANCE(ST_GeogPoint(c.longitude, c.latitude), ST_GeogPoint(s.long, s.lat)) AS distance
+  s.short_name,
+  s.address,
+  ST_DISTANCE(ST_GeogPoint(c.longitude, c.latitude), ST_GeogPoint(s.long, s.lat)) AS distance,
+  s.grade_cat
 FROM 
   {{ ref("stg_street_crimes") }}  c
 JOIN 
